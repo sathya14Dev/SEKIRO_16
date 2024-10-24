f33f2612134bef5f165908c89b904ef324a18cae
sathya14Dev <sathyabintang@gmail.com>
2024-10-24 18:24:20 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// Ambil elemen modal dengan ID 'id01'
+var modal1 = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
+
+// Ketika pengguna mengklik di luar modal, tutup modal tersebut
+window.onclick = function(event) {
+    // Periksa jika elemen yang diklik adalah modal
+    if (event.target === modal1) {
+        // Ubah gaya tampilan modal menjadi 'none' untuk menyembunyikannya
+        modal1.style.display = "none";
+    }
+    else if (event.target === modal2) {
+        // Ubah gaya tampilan modal menjadi 'none' untuk menyembunyikannya
+        modal2.style.display = "none";
+    }
+};
+
+history.pushState(null,null,window.location.href);
+
+window.openState = function() {
+    history.pushState(null,null,window.location.href)
+};
+
+const loginModal = document.getElementById('id01');
+const registerModal = document.getElementById('id02');
+const openRegister = document.getElementById('openRegister');
+const openLogin = document.getElementById('openLogin');
+
+openRegister.addEventListener('click', function (event) {
+    event.preventDefault();
+    loginModal.style.display = 'none';
+    registerModal.style.display = 'block';
+});
+
+openLogin.addEventListener('click', function (event) {
+    event.preventDefault();
+    loginModal.style.display = 'block';
+    registerModal.style.display = 'none';
+});
---
+++ scriptporto.js
@@ -0,0 +1,11 @@
+function lihatel() {
+  document.getElementById('tableel').innerHTML= 'Halo Nama Saya ELIJAH'
+};
+
+function lihatsuma() {
+  document.getElementById('tablesuma').innerHTML= 'Halo Nama Saya KUSUMA'
+};
+
+function lihatsathya() {
+  document.getElementById('tablesathya').innerHTML= 'Halo Nama Saya SATHYA'
+};
